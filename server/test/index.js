0c547164904e34e547b15b4216198cb274f555f6
Daniel Turner <dturner@tcd.ie>
2017-01-23 18:53:43 +0000

    add more descriptive suite

--- server/test/index.js
+++ server/test/index.js
@@ -3,23 +3,25 @@ import assert from 'assert';
 import winston from 'winston';
 import app, {logger} from '../src/app';
 
+// remove logger for testing
 process.env.NODE_ENV = 'test';
 logger.remove(winston.transports.Console);
 
+describe('Endpoint "/"', () => {
+  describe('GET', () => {
+    it('should return Hello World', (done) => {
+      request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-type', /text\/html/)
+      .end((err, res) => {
+        const expectedBody = 'Hello World';
+        const actualBody = res.text;
 
-describe('Endpoint /', () => {
-  it('should return Hello World', (done) => {
-    request(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-type', /text\/html/)
-    .end((err, res) => {
-      const expectedBody = 'Hello World';
-      const actualBody = res.text;
-
-      assert.equal(actualBody, expectedBody, 'Body retrieved successfully');
-      assert.equal(err, null);
-      done();
+        assert.equal(actualBody, expectedBody, 'Body retrieved successfully');
+        assert.equal(err, null);
+        done();
+      });
     });
   });
 });
