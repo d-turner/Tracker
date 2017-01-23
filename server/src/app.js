aba83c2e251d0fb53326749ab31a06a61df6a27f
Daniel Turner <dturner@tcd.ie>
2017-01-23 10:55:06 +0000

    add morgan to middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
 // import npm packages
 import express from 'express';
+import morgan from 'morgan';
+import winston from 'winston';
 
 // init app
 const app = express();
+
+app.use(morgan('dev'));
 
 // test method
 app.get('/', (req, res) => {
@@ -10,9 +14,11 @@ app.get('/', (req, res) => {
 });
 
 // catch all other requests
-app.use((err, req, res) => {
-  console.error(err.stack);
-  res.send(err);
+app.use((err, req, res, next) => {
+  winston.log('error', err.stack);
+  // if (err) res.send(err);
+  res.status(500).send('Not working');
 });
 
 export default app;
+export {winston, morgan};
