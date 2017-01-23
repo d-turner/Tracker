48d313b5b31e69baf5eba66973556092691ed4ab
Daniel Turner <dturner@tcd.ie>
2017-01-23 11:47:43 +0000

    Added modular logger from util and removed morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
-import app, {winston, morgan} from './app';
+import app, {logger} from './app';
 
 app.listen(8080, function() {
   const host = this.address().address;
   const port = this.address().port;
-  winston.log('info', `Started listening at http://${host}:${port}`);
+  logger.info(`Started listening at http://${host}:${port}`);
 });
 
-process.on('uncaughtException', err => console.error('uncaught exception: ', err)); // thrown exceptions
-process.on('unhandledRejection', err => console.error('unhandled rejection: ', err)); // unhandled promises
+process.on('uncaughtException', err => logger.error('uncaught exception: ', err)); // thrown exceptions
+process.on('unhandledRejection', err => logger.error('unhandled rejection: ', err)); // unhandled promises
