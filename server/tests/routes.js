82392ccd3073718a9c7ca937d27d86b8ba4503f0
Daniel Turner <dturner@tcd.ie>
2017-01-24 14:17:28 +0000

    add tests for the new routing and errorHandler

--- server/tests/routes.js
+++ server/tests/routes.js
@@ -18,7 +18,59 @@ describe('Endpoint "/"', () => {
         const expectedBody = 'Hello World';
         const actualBody = res.text;
 
-        assert.equal(actualBody, expectedBody, 'Body retrieved successfully');
+        assert.equal(actualBody, expectedBody, 'Body retrieval unsuccessful');
+        assert.equal(err, null);
+        done();
+      });
+    });
+  });
+});
+
+
+describe('Endpoint "/anythingElse"', () => {
+  describe('GET', () => {
+    it('should return a 404 error', (done) => {
+      request(app)
+      .get('/anythingElse')
+      .expect(404)
+      .expect('Content-type', /text\/html/)
+      .end((err, res) => {
+        const expectedBody = '404: File not found.';
+        const actualBody = res.text;
+
+        assert.equal(actualBody, expectedBody, 'Body retrieval unsuccessful');
+        assert.equal(err, null);
+        done();
+      });
+    });
+  });
+  describe('Simulate a database error', () => {
+    it('should return a 500 error', (done) => {
+      request(app)
+      .get('/simulate')
+      .expect(500)
+      .expect('Content-type', /text\/html/)
+      .end((err, res) => {
+        const expectedBody = 'Sorry something went wrong';
+        const actualBody = res.text;
+
+        assert.equal(actualBody, expectedBody, 'Body retrieval unsuccessful');
+        assert.equal(err, null);
+        done();
+      });
+    });
+  });
+  describe('POST', () => {
+    it('should return a 403 error', (done) => {
+      request(app)
+      .post('/anythingElse')
+      .expect(403)
+      .expect('Content-type', /text\/html/)
+      .end((err, res) => {
+        const expectedBody = 'Forbidden: POST';
+        const actualBody = res.text;
+
+        assert.equal(actualBody, expectedBody, 'Body retrieval unsuccessful');
         assert.equal(err, null);
         done();
       });
