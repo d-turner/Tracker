92d7f2190ffbb83917a5b3681ee3768dafaebe48
Daniel Turner <dturner@tcd.ie>
2017-01-25 23:01:48 +0000

    add public static folder to middleware

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(morgan('combined', {stream: logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use(express.static(__dirname.join('/public')));
+app.use(express.static(__dirname.join('/public')));
 
 // index method
 app.get('/', (req, res) => {
